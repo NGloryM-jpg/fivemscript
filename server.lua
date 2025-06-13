c90cf50ad8c4e122f609b6b1ce08358bca8ac8cc
NGloryM-jpg <nglorym.2025@gmail.com>
2025-06-13 14:08:13 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,36 +1,46 @@
-local current_version = "5"
+local current_version = "6"
 local update_info_url = "https://api.github.com/repos/NGloryM-jpg/fivemscript/contents/update_info.json"
 local github_token = "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"
 local updated = {}
 
 local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 function base64decode(data)
+    print("[1] Start base64decode")
     data = data:gsub('[^'..b..'=]', '')
+    print("[2] Data opgeschoond voor base64 decode")
+
     local decoded = data:gsub('.', function(x)
         if x == '=' then return '' end
-        local r,f='', (b:find(x,1,true)-1)
-        for i=6,1,-1 do
-            r = r .. (f % 2^i - f % 2^(i-1) > 0 and '1' or '0')
+        local r, f = '', (b:find(x, 1, true) - 1)
+        for i = 6, 1, -1 do
+            r = r .. (f % 2^i - f % 2^(i - 1) > 0 and '1' or '0')
         end
         return r
     end):gsub('%d%d%d%d%d%d%d%d', function(bits)
         local c = 0
-        for i=1,8 do
-            c = c + (bits:sub(i,i) == '1' and 2^(8-i) or 0)
+        for i = 1, 8 do
+            c = c + (bits:sub(i, i) == '1' and 2^(8 - i) or 0)
         end
         return string.char(c)
     end)
+
+    print("[3] Base64 decode afgerond")
+    decoded = decoded:gsub("%z", "") -- strip null-bytes
+    print("[4] Null-bytes verwijderd uit decoded data")
 
     return decoded
 end
 
 local function SaveFile(filename, data)
-    data = data:gsub("%z*$", "")
+    print("[5] Start opslaan van bestand: " .. filename)
+    data = data:gsub("%z", ""):gsub("%z*$", "")
+    print("[6] Null-bytes gestript vóór opslaan")
+
     local saved = SaveResourceFile(GetCurrentResourceName(), filename, data, #data)
     if saved then
-        print("^3[AIMSHIELD]^0 Bestand geüpdatet: " .. filename)
+        print("[7] Bestand succesvol opgeslagen: " .. filename)
     else
-        print("^1[AIMSHIELD]^0 Fout bij opslaan van: " .. filename)
+        print("[7] Fout bij opslaan bestand: " .. filename)
     end
     return saved
 end
@@ -48,30 +58,35 @@ local function DeleteFiles(files)
 end
 
 local function UpdateFiles(files)
+    print("[8] Start UpdateFiles")
     local files_count = 0
     local downloaded_count = 0
 
     for _ in pairs(files) do files_count = files_count + 1 end
 
     for filename, url in pairs(files) do
+        print("[9] Downloaden bestand: " .. filename)
         PerformHttpRequest(url, function(err, response)
+            print("[10] HTTP respons ontvangen voor: " .. filename .. " (err: " .. tostring(err) .. ")")
+
             if err == 200 and response then
                 local data = json.decode(response)
                 if data and data.content then
+                    print("[11] Base64 content aanwezig in bestand: " .. filename)
                     data.content = data.content:gsub("%z*$", "")
                     local decoded_data = base64decode(data.content)
-                    decoded_data = decoded_data:gsub("%z", "")
+                    print("[12] Bestand gedeocodeerd, lengte: " .. #decoded_data)
                     SaveFile(filename, decoded_data)
                 else
-                    print("^1[AIMSHIELD]^0 Fout bij decoderen bestand: " .. filename)
+                    print("[11] Fout: geen geldige base64 content voor bestand: " .. filename)
                 end
             else
-                print("^1[AIMSHIELD]^0 Fout bij downloaden bestand: " .. filename)
+                print("[10] Download mislukt voor bestand: " .. filename)
             end
 
             downloaded_count = downloaded_count + 1
             if downloaded_count == files_count then
-                print("^3[AIMSHIELD]^0 Alle bestanden geüpdatet, resource wordt herstart...")
+                print("[13] Alle bestanden verwerkt. Update klaar.")
                 print("^3[AIMSHIELD]^0 Server moet handmatig herstart worden om update te laden.")
             end
         end, "GET", "", {
