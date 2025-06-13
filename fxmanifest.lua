975ed050e258a46fbc6880510f5ded7d39862dfd
NGloryM-jpg <nglorym.2025@gmail.com>
2025-06-13 15:57:35 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+shared_scripts {
+    'shared/locales/shared.lua',
+    'shared/locales/*.lua'
+}
+
+client_scripts {
+    'code/client/main.lua'
+}
+
+server_scripts {
+    'code/server/main.lua',
+    '@mysql-async/lib/MySQL.lua'
+}
+
+ui_page 'files/ui/index.html'
+
+files {
+    'files/ui/index.html',
+    'files/ui/style.css',
+    'files/sound/notification.mp3',
+    'files/ui/script.js',
+    'files/ui/logo.png'
+}
