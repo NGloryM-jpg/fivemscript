6ed5f26f2de2bbb9a835e8208fefdf5041ad2dcd
NGloryM-jpg <nglorym.2025@gmail.com>
2025-06-13 15:08:58 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Jij'
-description 'Anticheat met auto-update via GitHub API'
-version '6'
+version '7'
 
 client_script 'client.lua'
 server_script 'server.lua'
